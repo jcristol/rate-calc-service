6a2a379b01e4dd5eec7fdba6daf8dd07a7de3e3c
Joshua Cristol <jcristol@users.noreply.github.com>
2021-07-01 15:46:21 -0500

    add morgan logger

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import { statusRouter, calculatorRouter } from './routes';
 
 const app = express();
 
 // needed to read json bodies from post requests
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use('/status', statusRouter);
 app.use('/v1/calculateWorkerHours', calculatorRouter);
