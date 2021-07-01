e2bfebb6ae972c2b2c273015e7bdb37a592ce066
Joshua Cristol <jcristol@users.noreply.github.com>
2021-07-01 15:17:04 -0500

    add validator

--- src/server/app.ts
+++ src/server/app.ts
@@ -8,4 +8,6 @@ app.use(express.json());
 app.use('/status', statusRouter);
 app.use('/v1/calculateWorkerHours', calculatorRouter);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
--- src/server/routes/calculator.ts
+++ src/server/routes/calculator.ts
@@ -1,63 +1,15 @@
 import { Router } from 'express';
-
-interface IWorkerTimeSheet {
-  workerHours: IWorkRecord[];
-  config: ICalculatorConfig;
-}
-
-interface ICalculatorConfig {
-  workWeekStart: string;
-  workerHourlyBaseRate: number;
-}
-
-interface IWorkRecord {
-  date: string;
-  hours: number;
-}
-
-interface IWorkWeekSummary {
-  regularHours: number;
-  overtimeHours: number;
-  regularHoursGrossPay: number;
-  overtimeHoursGrossPay: number;
-  totalGrossPay: number;
-}
-
-function buildWorkerTimeSheet(
-  rawData: Record<string, unknown>
-): IWorkerTimeSheet {
-  if (typeof rawData.workWeekStart !== 'string') {
-    throw new Error('workWeekStart was not a string');
-  }
-  if (typeof rawData.workerHourlyBaseRate !== 'string') {
-    throw new Error('workWeekStart was not a string');
-  }
-  if (!Array.isArray(rawData.workerHours)) {
-    throw new Error('workerHours is not an array');
-  }
-  const config = {
-    workWeekStart: rawData.workWeekStart,
-    workerHourlyBaseRate: parseInt(rawData.workerHourlyBaseRate)
-  };
-  const workerHours = rawData.workerHours.map((elem) => {
-    if (typeof elem.date !== 'string') {
-      throw new Error('workerHours.date was not a string');
-    }
-    if (typeof elem.hours !== 'string') {
-      throw new Error('workerHours.hours was not a string');
-    }
-    return { date: elem.date, hours: parseInt(elem.hours) };
-  });
-  return {
-    config,
-    workerHours
-  };
-}
+import {
+  IWorkerTimeSheet,
+  IWorkRecord,
+  IWorkWeekSummary,
+  validate
+} from './types';
 
 function groupWorkByWeek(workerTimeSheet: IWorkerTimeSheet): [[IWorkRecord]] {
   const {
     workerHours,
-    config: { workWeekStart }
+    configuration: { workWeekStart }
   } = workerTimeSheet;
   // record from the front of the list
   const firstRecord = workerHours.shift();
@@ -92,7 +44,9 @@ function summarizeWorkWeek(
   hourlyRate: number,
   overtimeRate = 1.5
 ): IWorkWeekSummary {
-  const totalHoursWorked = week.reduce((acc, day) => acc + day.hours, 0);
+  const totalHoursWorked = week
+    .map((day) => parseInt(day.hours))
+    .reduce((acc, hours) => acc + hours, 0);
   const regularHours = Math.min(totalHoursWorked, 40);
   const overtimeHours = Math.max(totalHoursWorked - 40, 0);
   const regularHoursGrossPay = regularHours * hourlyRate;
@@ -108,11 +62,12 @@ function summarizeWorkWeek(
 
 export const calculatorRouter = Router();
 calculatorRouter.post('/', (req, res) => {
-  const workerTimeSheet = buildWorkerTimeSheet(req.body);
+  const workerTimeSheet = validate(req.body);
   const workByWeek = groupWorkByWeek(workerTimeSheet);
   const response = workByWeek.map((week) => {
     const [firstDay] = week;
-    const workerHourlyBaseRate = workerTimeSheet.config.workerHourlyBaseRate;
+    const workerHourlyBaseRate =
+      workerTimeSheet.configuration.workerHourlyBaseRate;
     return {
       workWeek: firstDay.date,
       summary: summarizeWorkWeek(week, workerHourlyBaseRate)
