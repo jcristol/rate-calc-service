20a69abf02daf4c0f10376b92310be79215f1043
Joshua Cristol <jcristol@users.noreply.github.com>
2021-07-04 14:21:43 -0500

    write groupByWeekTests

--- tests/calculateWorkerHours.spec.ts
+++ tests/calculateWorkerHours.spec.ts
@@ -1,94 +1,9 @@
 import { groupWorkByWeek } from '../api/v1/calculateWorkerHours';
-// todo: tests
-// const sampleWorkerHours = [
-//   {
-//     date: '2021-05-03',
-//     hours: '0'
-//   },
-//   {
-//     date: '2021-05-04',
-//     hours: '0'
-//   },
-//   {
-//     date: '2021-05-05',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-06',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-07',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-08',
-//     hours: '4'
-//   },
-//   {
-//     date: '2021-05-09',
-//     hours: '0'
-//   },
-//   {
-//     date: '2021-05-10',
-//     hours: '10'
-//   },
-//   {
-//     date: '2021-05-11',
-//     hours: '10'
-//   },
-//   {
-//     date: '2021-05-12',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-13',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-14',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-15',
-//     hours: '4'
-//   },
-//   {
-//     date: '2021-05-16',
-//     hours: '0'
-//   },
-//   {
-//     date: '2021-05-17',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-18',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-19',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-20',
-//     hours: '8'
-//   },
-//   {
-//     date: '2021-05-21',
-//     hours: '10'
-//   },
-//   {
-//     date: '2021-05-22',
-//     hours: '0'
-//   },
-//   {
-//     date: '2021-05-23',
-//     hours: '0'
-//   }
-// ];
+import {sampleGroupedByWeekMondayStart, sampleGroupedByWeekTuesdayStart, sampleWorkerHours} from './fixtures/workerHours';
+
 
 describe('groupWorkByWeek', () => {
-  it('throw an Error when there are no workerHours', () => {
+  it('throw an Error when there isnt work in the time sheet', () => {
     const timeSheet = {
       configuration: {
         workWeekStart: 'Monday',
@@ -98,17 +13,36 @@ describe('groupWorkByWeek', () => {
     };
     expect(() => groupWorkByWeek(timeSheet)).toThrow(Error);
   });
-  it('group week data with Monday as the start date', () => {
-    // const timeSheet = {
-    //   configuration: {
-    //     workWeekStart: 'Monday',
-    //     workerHourlyBaseRate: 45
-    //   },
-    //   workerHours: sampleWorkerHours
-    // };
-    // const
-    // expect there to be three top level array with the proper number of entries
-    // expect the data of the output arrary to be the proper shape
+  it.only('should group data by week with the start of each week as Monday', () => {
+    const timeSheet = {
+      configuration: {
+        workWeekStart: 'Monday',
+        workerHourlyBaseRate: 45,
+      },
+      workerHours: sampleWorkerHours,
+    };
+    const groupedByMonday = groupWorkByWeek(timeSheet);
+    groupedByMonday.forEach((week, weekIndex) => {
+      week.forEach((day, dayIndex) => {
+        expect(day.date).toBe(sampleGroupedByWeekMondayStart[weekIndex][dayIndex].date);
+        expect(day.hours).toBe(sampleGroupedByWeekMondayStart[weekIndex][dayIndex].hours);
+      })
+    })
   });
-  // it('group week data with Wednesday as the start date', () => {});
+  it('group week data with Wednesday as the start date', () => {
+    const timeSheet = {
+      configuration: {
+        workWeekStart: 'Tuesday',
+        workerHourlyBaseRate: 45,
+      },
+      workerHours: sampleWorkerHours,
+    };
+    const groupedByTuesday = groupWorkByWeek(timeSheet);
+    groupedByTuesday.forEach((week, weekIndex) => {
+      week.forEach((day, dayIndex) => {
+        expect(day.date).toBe(sampleGroupedByWeekTuesdayStart[weekIndex][dayIndex].date);
+        expect(day.hours).toBe(sampleGroupedByWeekTuesdayStart[weekIndex][dayIndex].hours);
+      })
+    })
+  });
 });
